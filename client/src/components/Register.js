bed08088d7120b0d991398975919ed49a5bb7516
stobon17 <simontobon7@gmail.com>
2024-03-20 15:58:05 -0500

    convert register form to bootstrap form control

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -16,11 +16,11 @@ const Register = () => {
         e.preventDefault();
         try {
             const res = await axios.post('http://localhost:5000/users', JSON.stringify(formData),
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
             console.log(res.data); // Handle success or redirect to login page
         } catch (err) {
             console.error(err.response.data);
@@ -28,15 +28,22 @@ const Register = () => {
     };
 
     return (
-        <div>
-            <h2>Register</h2>
-            <form onSubmit={e => onSubmit(e)}>
-                <input type="text" placeholder="Username" name="username" value={username} onChange={e => onChange(e)} required />
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-                <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minLength="6" required />
-                <button type="submit">Register</button>
+        <div class="container">
+            <h2 class="mt-5">Register</h2>
+            <form onSubmit={e => onSubmit(e)} class="mt-3">
+                <div class="mb-3">
+                    <input type="text" class="form-control" placeholder="Username" name="username" value={username} onChange={e => onChange(e)} required />
+                </div>
+                <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minLength="6" required />
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
             </form>
         </div>
+
     );
 };
 
